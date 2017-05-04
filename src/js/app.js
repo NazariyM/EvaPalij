1c0408bcaec4bcd0370207e19223540156d7df46
Nazariy Mokriy <nazarriko@gmail.com>
2017-05-04 09:30:38 +0300

    up

---
+++ src/js/app.js
@@ -0,0 +1,6 @@
+//=include lib/jquery.min.js
+//=include lib/scrolloverflow.min.js
+//=include lib/fullpage.min.js
+//=include lib/svgxuse.min.js
+//=include components/popup.js
+//=include common.js
