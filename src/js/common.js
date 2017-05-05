1412acc3174ec6559c35a26253d75e9b8988aba0
Nazariy Mokriy <nazarriko@gmail.com>
2017-05-05 09:51:49 +0300

    fixes

--- src/js/common.js
+++ src/js/common.js
@@ -115,19 +115,13 @@ $(function () {
 	function initBlackHeader() {
 		let $header = $('.js-header'),
 			$nav = $('.js-nav'),
-			$body = $('body'),
 			windowWidth = 767;
 
 		$(window).on('resize, load', function () {
-			setTimeout(function () {
 				if ($(window).width() <= windowWidth && $header.hasClass('header_inside')) {
 					$header.removeClass('header_inside');
 					$nav.removeClass('nav_inside');
-				} else if ($(window).width() >= windowWidth && !$header.hasClass('header_inside') && !$body.hasClass('is-home')) {
-					$header.addClass('header_inside');
-					$nav.addClass('nav_inside');
 				}
-			},200);
 		});
 	}
 
